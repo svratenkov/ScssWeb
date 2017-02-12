5510f741c69e97826a2cbc0c79a0bf9ef0907e47
svratenkov <svratenkov@projectbureau.ru>
2017-02-12 11:56:02 +0300

    README.md Images shrinked

--- src/views/footer.php
+++ src/views/footer.php
@@ -1,7 +1,7 @@
 <div class="container-fluid">
 	<div class="row bg-info">
 		<div class="col-md-12">
-			<p>&copy;Micron, 2016</p>
+			<p>&copy;<a href="https://github.com/svratenkov/scssweb">ScssWeb</a>, 2017</p>
 		</div>
 	</div>
 </div>
